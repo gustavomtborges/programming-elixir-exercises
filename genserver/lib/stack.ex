04c6f7c74118dfbc49d5bff93dc26bf808845d0e
Gustavo M T Borges <gustavo.mtborges@gmail.com>
2020-04-17 22:15:06 -0300

    feat: add cast function to genserver

--- genserver/lib/stack.ex
+++ genserver/lib/stack.ex
@@ -8,4 +8,8 @@ defmodule Stack.Server do
   def handle_call(:pop, _from, [head | tail]) do
     {:reply, head, tail}
   end
+
+  def handle_cast({:push, new_value}, current_stack) do
+    {:noreply, current_stack ++ [new_value]}
+  end
 end
